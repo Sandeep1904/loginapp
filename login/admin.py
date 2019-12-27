d6d1386f6fb5b898aa24bfc8f24c822b37ae9c23
Sandeep Hugar <sandeephugar19@gmail.com>
2019-12-27 12:54:01 +0530

    new login

---
+++ login/admin.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+from django.contrib import admin
+
+# Register your models here.
+# -*- coding: utf-8 -*-
+
+
+from django.contrib import admin
+from login.models import UserProfileInfo, User
+# Register your models here.
+admin.site.register(UserProfileInfo)
