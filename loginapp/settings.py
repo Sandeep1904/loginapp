cf975d5a7148e3773fb23efe31b71f3a5d27182e
Sandeep Hugar <sandeephugar19@gmail.com>
2019-12-27 17:35:07 +0530

    dsa

--- loginapp/settings.py
+++ loginapp/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'loginapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
